759c93678d0cbe84af0f91ebc883e2d28ec768f5
Nehemias Lugo <68383194+GigaPikachu@users.noreply.github.com>
2024-05-20 16:03:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [pantalla_inicial, menu_personajes, Game],
+  scene: [Game, pantalla_inicial, menu_personajes, ],
 };
 
 // Create a new Phaser game instance
